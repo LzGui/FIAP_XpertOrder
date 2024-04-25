8691ad5ff1e376f2e3b9fe90bde5ef94b77620b5
lzgui <lzguilhermecp@gmail.com>
2024-04-25 16:54:00 -0300

    Validando se um produto existe

--- XpertOrder-mspedidos/src/main/java/br/com/fiap/xpertorder/xpertordermspedidos/service/PedidoService.java
+++ XpertOrder-mspedidos/src/main/java/br/com/fiap/xpertorder/xpertordermspedidos/service/PedidoService.java
@@ -1,9 +1,19 @@
 package br.com.fiap.xpertorder.xpertordermspedidos.service;
 
+import br.com.fiap.xpertorder.xpertordermspedidos.model.ItemPedido;
 import br.com.fiap.xpertorder.xpertordermspedidos.model.Pedido;
 import br.com.fiap.xpertorder.xpertordermspedidos.repository.PedidoRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class PedidoService {
@@ -11,7 +21,45 @@ public class PedidoService {
    @Autowired
    private PedidoRepository pedidoRepository;
 
+   @Autowired
+   private RestTemplate restTemplate;
+
+   @Autowired
+   private ObjectMapper objectMapper;
+
    public Pedido criarPedido(Pedido pedido) {
+      boolean produtosDisponiveis = verificarDisponibilidadeProdutos(pedido.getItensPedido());
+
       return pedidoRepository.save(pedido);
+   }
+   
+   private boolean verificarDisponibilidadeProdutos(List<ItemPedido> itensPedido){
+      for (ItemPedido itemPedido : itensPedido) {
+         Integer idProduto = itemPedido.getIdProduto();
+         int quantidade = itemPedido.getQuantidade();
+
+         ResponseEntity<String> resp = restTemplate.getForEntity(
+                 "http://localhost:8080/api/produtos/{produtoId}",
+                 String.class,
+                 idProduto
+         );
+
+         if (resp.getStatusCode() == HttpStatus.NOT_FOUND) {
+            throw new NoSuchElementException("Produto não encontrado");
+         } else {
+            try {
+               JsonNode produtoJson = objectMapper.readTree((resp.getBody()));
+               int quantidadeEstoque = produtoJson.get("quantidade_estoque").asInt();
+
+               if (quantidadeEstoque < quantidade) {
+                  return false;
+               }
+            } catch (IOException e) {
+               // tratar dps
+            }
+         }
+      }
+
+      return true;
    }
 }
