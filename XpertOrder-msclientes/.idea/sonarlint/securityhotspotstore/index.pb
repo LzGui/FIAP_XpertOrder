ab34b7478907e4ac870bf3b396574055f583ba4e
lzgui <lzguilhermecp@gmail.com>
2024-04-29 20:19:53 -0300

    modificacoes msclientes

--- XpertOrder-msclientes/.idea/sonarlint/securityhotspotstore/index.pb
+++ XpertOrder-msclientes/.idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,5 @@ Lsrc/main/java/br/com/fiap/xpertorder/xpertordermsclientes/model/Cliente.java,a
 [src/main/java/br/com/fiap/xpertorder/xpertordermsclientes/repository/ClienteRepository.java,c\3\c37be4e5397ab2ce316934ee1e46e6766ea422be
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+…
+Usrc/main/java/br/com/fiap/xpertorder/xpertordermsclientes/service/ClienteService.java,b\9\b91333e7cbe27a87e7c9512e9ff15a183b7f9df6
--- XpertOrder-msclientes/src/main/java/br/com/fiap/xpertorder/xpertordermsclientes/ClientesApplication.java
+++ XpertOrder-msclientes/src/main/java/br/com/fiap/xpertorder/xpertordermsclientes/ClientesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XpertOrderMsclientesApplication {
+public class ClientesApplication {
 
    public static void main(String[] args) {
-      SpringApplication.run(XpertOrderMsclientesApplication.class, args);
+      SpringApplication.run(ClientesApplication.class, args);
    }
 
 }
